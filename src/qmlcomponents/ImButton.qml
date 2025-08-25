raspberrypi/rpi-imager

Unraid Branding (#15)
* main window in pretty good shape - now onto the popup boxes

* hw popup + delegate, os popup + delegate, and dst popup + delegate pretty much in a good spot. based on how the icons are displayed, didn't end up needing orange pngs, because the dark ones can be changed using ColorOverlay

* about to start working on options popup

* think the settings menu seems okay now

* language/keyboard selector & progress bar theming

* removal of a few now unused rpi pngs

* replace instances of "#2B2A29" with new UnColors singleton member mediumGray

* replace instances of "#FF8C2F" with new UnColors singleton member orange

* replace instances of "#1C1B1B" with new UnColors singleton member darkGray

* replace instances of "#FFFFFF" with color name white

src/qmlcomponents/ImButton.qml
@@ -10,8 +10,25 @@ import QtQuick.Controls.Material 2.2
 
 Button {
     font.family: roboto.name
-    Material.background: activeFocus ? "#d1dcfb" : "#ffffff"
-    Material.foreground: "#cd2355"
+    font.bold: true
+    background: Rectangle {
+        implicitWidth: 100
+        implicitHeight: 40
+        opacity: enabled ? 1.0 : 0.3
+        color: parent.hovered ? UnColors.orange : UnColors.darkGray
+        border.color: UnColors.orange
+        border.width: 1
+        radius: 25
+    }
+    contentItem: Text {
+        text: parent.text
+        font: parent.font
+        opacity: enabled ? 1.0 : 0.3
+        color: parent.hovered ? UnColors.darkGray : "white"
+        horizontalAlignment: Text.AlignHCenter
+        verticalAlignment: Text.AlignVCenter
+        elide: Text.ElideRight
+    }
     Accessible.onPressAction: clicked()
     Keys.onEnterPressed: clicked()
     Keys.onReturnPressed: clicked()

unraid/usb-creator-next

src/qmlcomponents/ImButton.qml
@@ -10,12 +10,34 @@ import QtQuick.Controls.Material 2.2
 import RpiImager
 
 Button {
+
+    // This is the current version. I replaced it with Unraid Button - Ajit
+    // font.family: Style.fontFamily
+    //     font.capitalization: Font.AllUppercase
+    //     Material.background: activeFocus ? Style.buttonFocusedBackgroundColor : Style.buttonBackgroundColor
+    //     Material.foreground: Style.buttonForegroundColor
+    //    Material.roundedScale: Material.ExtraSmallScale
+
     font.family: Style.fontFamily
-    font.capitalization: Font.AllUppercase
-    Material.background: activeFocus ? Style.buttonFocusedBackgroundColor : Style.buttonBackgroundColor
-    Material.foreground: Style.buttonForegroundColor
-    Material.roundedScale: Material.ExtraSmallScale
-    activeFocusOnTab: true
+    font.bold: true
+    background: Rectangle {
+        implicitWidth: 100
+        implicitHeight: 40
+        opacity: enabled ? 1.0 : 0.3
+        color: parent.hovered ? Style.unraidAccentColor : Style.unraidPrimaryBgColor
+        border.color: Style.unraidAccentColor
+        border.width: 1
+        radius: 25
+    }
+    contentItem: Text {
+        text: parent.text
+        font: parent.font
+        opacity: enabled ? 1.0 : 0.3
+        color: parent.hovered ? Style.unraidTextFocusColor : "white"
+        horizontalAlignment: Text.AlignHCenter
+        verticalAlignment: Text.AlignVCenter
+        elide: Text.ElideRight
+    }
     Accessible.onPressAction: clicked()
     Keys.onEnterPressed: clicked()
     Keys.onReturnPressed: clicked()